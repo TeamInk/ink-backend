8bd5cc5ce5eae092a216d7f0f6129e1062534b13
hkpark02 <hkpark02@cafe24corp.com>
2024-05-19 17:42:33 +0900

    Fix: Mail 계정 프로퍼티에서 가져오도록 수정

--- ink-admin/src/test/java/net/ink/admin/service/AdminMemberServiceTest.java
+++ ink-admin/src/test/java/net/ink/admin/service/AdminMemberServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import javax.mail.internet.MimeMessage;
@@ -99,7 +100,7 @@ class AdminMemberServiceTest {
 
         adminMemberService.promoteAdminMemberById(1L);
 
-        verify(javaMailSender, times(1)).send(any(MimeMessage.class));
+        verify(javaMailSender, times(1)).send(any(SimpleMailMessage.class));
     }
 
 }
